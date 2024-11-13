55b76ecb742476c68f8162a3dae2b103254e700a
Yousef emad mikhail <mad33833@gmail.com>
2024-11-13 10:46:46 +0200

    add category model to database

---
+++ backend/DB/models/category.model.js
@@ -0,0 +1,27 @@
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image:{
+        type: String
+    },
+    addedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    updatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: false
+    }
+}
+,{timestamps: true});
+
+const Category = mongoose.models.Category || model('Category',categorySchema)
+export default Category;
