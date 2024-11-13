22ef8b73fa68fddacda1ab058ab27920d8757898
Yousef emad mikhail <mad33833@gmail.com>
2024-11-13 10:30:39 +0200

    add env file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import { config } from 'dotenv';
+
+// Load environment variables from.env file
+config({path:"./config/dev.env"})
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, ()=>{
     console.log(`app is running on port ${port}`)});
