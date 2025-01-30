e7e021c7bf98073bd64a71d1a420ce7576d3bed3
Yousef emad mikhail <mad33833@gmail.com>
2025-01-30 10:53:01 +0200

    add multer middleware for cloud storage

--- backend/SRC/middlewares/multer.js
+++ backend/SRC/middlewares/multer.js
@@ -48,3 +48,31 @@ export const multerMiddleLocal = ({
     const file = multer({ fileFilter, storage })
     return file
 }
+
+
+
+export const multerMiddleHost = ({
+    extensions = allowedExtensions.image,
+}) => {
+
+
+    // diskStorage
+    const storage = multer.diskStorage({
+        filename: (req, file, cb) => {
+            const uniqueFileName = generateUniqueString(6) + '_' + file.originalname
+            cb(null, uniqueFileName)
+        }
+    })
+
+    // file Filter
+    const fileFilter = (req, file, cb) => {
+        if (extensions.includes(file.mimetype.split('/')[1])) {
+            return cb(null, true)
+        }
+        cb(new Error('Image format is not allowed!'), false)
+    }
+
+
+    const file = multer({ fileFilter, storage })
+    return file
+}
