2fccc256a291e8efcc2fd141b43404677a199bf5
Yousef emad mikhail <mad33833@gmail.com>
2025-02-05 15:30:16 +0200

    add dummy data to categories module

--- backend/SRC/modules/Category/category.controller.js
+++ backend/SRC/modules/Category/category.controller.js
@@ -1,6 +1,7 @@
 import slugify from "slugify";
 import Category from "../../../DB/models/category.model.js";
 import { uploadFile } from "../../utils/cloudinary.utils.js";
+import axios from "axios";
 
 export const createCategory = async(req,res,next)=>{
     const user=req.user;
@@ -50,4 +51,31 @@ export const getCategory=async(req,res,next)=>{
     const category=await Category.findOne(queryFilter);
     if(!category)return next(new Error(`Category not found`,{cause:404}));
     return res.status(200).json({sucess:true,category})
+}
+
+export const addMealDB=async(req,res,next)=>{
+    const user=req.user;
+    const response = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php");
+    for(const category of response.data.categories){
+        const {strCategory,strCategoryThumb,strCategoryDescription}=category
+        const slug=strCategory
+        const isCategoryExists=Category.findOne({name:strCategory})
+        if(isCategoryExists) continue;
+        const { secure_url, public_id } = await uploadFile({
+            file: strCategoryThumb,
+            folder: `${process.env.UPLOADS_FOLDER}/Categories/${slug}`,
+        });
+        const categoryObj={
+            name:slug,
+            description:strCategoryDescription,
+            addedBy:user._id,
+            image:{
+                public_id:public_id,
+                secure_url:secure_url
+            }
+        }
+        await Category.create(categoryObj);
+    }
+    const categoires=await Category.find()
+    res.status(201).json({message:"categories added successfully",categoires});
 }
--- backend/SRC/modules/Category/category.router.js
+++ backend/SRC/modules/Category/category.router.js
@@ -6,10 +6,12 @@ import { allowedExtensions } from "../../utils/allowed-extensions.js";
 import { getDocumentByName } from "../../middlewares/finders.middleware.js";
 import Category from "../../../DB/models/category.model.js";
 import  expressAsyncHandler  from 'express-async-handler';
+import { systemRoles } from "../../utils/system-roles.js";
 
 const router =Router()
 router.post('/create',auth(),getDocumentByName(Category), multerMiddleHost(allowedExtensions.image).single('image'),expressAsyncHandler(categoryController.createCategory))
 router.get('/',expressAsyncHandler(categoryController.getAllCategories))
 router.get('/get-category',expressAsyncHandler(categoryController.getCategory))
+router.post('/add-mealdb',auth([systemRoles.ADMIN]),expressAsyncHandler(categoryController.addMealDB))
 
 export default router
