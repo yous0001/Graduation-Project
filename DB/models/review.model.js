7464fffe71cc974ec5d1ed6a85474d3dd0e9c3e0
Yousef emad mikhail <mad33833@gmail.com>
2025-04-10 18:03:34 +0200

    refactor review schema to handle all business needs

--- DB/models/review.model.js
+++ DB/models/review.model.js
@@ -1,25 +1,46 @@
 import mongoose, { model } from "mongoose";
 
 const reviewSchema = new mongoose.Schema({
-    userID:{ 
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true 
+    comment: {
+        type: String,
+        trim: true,
     },
-    recipeID:{ 
-        type: Schema.Types.ObjectId,
-        ref: 'Recipe' 
-    },
-    ingredientID:{
-        type: Schema.Types.ObjectId,
-        ref: 'Ingredient' 
-    },
-    rating:{ 
+    rate: {
         type: Number,
-        required: true 
+        min: 1,
+        max: 5,
+        required: true, 
     },
-    comment:String
-},{timestamps:true});
+    userID: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    recipe: {
+        type: mongoose.Types.ObjectId,
+        ref: "Recipe",
+        default: null,
+    },
+    ingredient: {
+        type: mongoose.Types.ObjectId,
+        ref: "Ingredient",
+        default: null,
+    },
+    likes: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    dislikes: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+}, {
+timestamps: true,
+});
 
 const Review = mongoose.models.Review || model('Review',reviewSchema)
 export default Review;
