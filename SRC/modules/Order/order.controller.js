2c7da27ee73e7fffe78e3941d4a9e707713feea9
Yousef emad mikhail <mad33833@gmail.com>
2025-06-27 01:33:52 +0300

    add address to get orders api

--- SRC/modules/Order/order.controller.js
+++ SRC/modules/Order/order.controller.js
@@ -184,13 +184,13 @@ export const cancelOrder=async(req,res,next)=>{
 
 export const getOrders=async(req,res,next)=>{
     const user=req.user
-    const orders=await Order.find({userId:user._id}).populate('items.ingredientId')
+    const orders=await Order.find({userId:user._id}).populate('items.ingredientId').populate('shippingAddressID')
     return res.status(200).json({orders})
 }
 
 export const getSpecificOrder=async(req,res,next)=>{
     const {id}=req.params
-    const order=await Order.findOne({_id:id}).populate('items.ingredientId')
+    const order=await Order.findOne({_id:id}).populate('items.ingredientId').populate('shippingAddressID')
     if(!order) return next(new Error("order not found", { cause: 404 }))
     if(order.userId.toString()!==req.user._id.toString()) return next(new Error("this is not one of your orders", { cause: 400 }))
     return res.status(200).json({order})
